85e4cbc326a869844d51b96cd234c532d7728065
ccsimpson3 <curt@metaclinic.com>
2020-12-11 15:52:42 -0600

    feat: index title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import Layout from 'components/layout';
+import React from "react";
+import Layout from "components/layout";
 import SEO from "components/seo";
-import { useStaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from "gatsby";
 
-import Section from 'sections/section';
+import Section from "sections/section";
 
 const Home = () => {
-
-    const data = useStaticQuery(graphql`
-        query {
-            contentfulLayout(slug: {eq: "/"}) {
-                id
-                title
-                description
-                contentful_id,
-                menu {
-                    name
-                    type
-                    menuItems {
-                        id
-                        title
-                        url
-                    }
-                },
-                contentModule {
-                    ... on Node {
-                        id
-                    }
-                }
-            }
+  const data = useStaticQuery(graphql`
+    query {
+      contentfulLayout(slug: { eq: "/" }) {
+        id
+        title
+        description
+        contentful_id
+        menu {
+          name
+          type
+          menuItems {
+            id
+            title
+            url
+          }
         }
-    `);
-    const menus = data.contentfulLayout.menu;
+        contentModule {
+          ... on Node {
+            id
+          }
+        }
+      }
+    }
+  `);
+  const menus = data.contentfulLayout.menu;
 
-    const contentModule = data.contentfulLayout.contentModule;
+  const contentModule = data.contentfulLayout.contentModule;
 
-    return (
-        <Layout menus={ menus }>
-            <SEO title="Homepage" />
-            {
-                contentModule.length > 0 && 
-                contentModule.map(content => (
-                    <Section contentModuleId={ content.id } type={ content.__typename } key={content.id}/>
-                ))
-            }
-        </Layout>
-    );
+  return (
+    <Layout menus={menus}>
+      <SEO title={data.contentfulLayout.title} />
+      {contentModule.length > 0 &&
+        contentModule.map(content => (
+          <Section
+            contentModuleId={content.id}
+            type={content.__typename}
+            key={content.id}
+          />
+        ))}
+    </Layout>
+  );
 };
 
 export default Home;
-
