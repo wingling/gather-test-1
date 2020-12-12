5c38a8c2f91fbb33fe8de0942350f626328d3da7
ccsimpson3 <curt@metaclinic.com>
2020-12-12 04:01:32 -0600

    feat: added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,56 +1,61 @@
 require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
-    siteMetadata: {
-        title: `GatherMed`,
-        description: `GatherMed is transforming practices for the new normal, driving meaningful change in outcomes and revenue for payers and practices across the world.`,
-        author: `GatherMed`,
+  siteMetadata: {
+    title: `GatherMed`,
+    description: `GatherMed is transforming practices for the new normal, driving meaningful change in outcomes and revenue for payers and practices across the world.`,
+    author: `GatherMed`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-185401096-1",
+        head: true,
+        pageTransitionDelay: 0,
+        defer: false,
+      },
     },
-    plugins: [
-        {
-            resolve: `gatsby-plugin-google-fonts`,
-            options: {
-              fonts: [
-                `material icons`,  
-                `Roboto\:400,700`,  
-                `Rubik\:400,700`
-              ],
-              display: 'swap'
-            }
-        },
-        "gatsby-plugin-postcss",
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-resolve-src",
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/assets/images`,
-            }
-        },
-        "gatsby-transformer-sharp",
-        "gatsby-plugin-sharp",
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Zenii Gatsby`,
-                short_name: `zenii`,
-                start_url: `/`,
-                background_color: `#6C63FF`,
-                theme_color: `#6C63FF`,
-                display: `minimal-ui`,
-                icon: `src/assets/images/favicon.png`,
-            }
-        },
-        {
-            resolve: `gatsby-source-contentful`,
-            options: {
-              spaceId: process.env.CONTENTFUL_SPACE_ID,
-              accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-            },
-          },
-          `@contentful/gatsby-transformer-contentful-richtext`,
-    ],
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`material icons`, `Roboto\:400,700`, `Rubik\:400,700`],
+        display: "swap",
+      },
+    },
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-resolve-src",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Zenii Gatsby`,
+        short_name: `zenii`,
+        start_url: `/`,
+        background_color: `#6C63FF`,
+        theme_color: `#6C63FF`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/favicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `@contentful/gatsby-transformer-contentful-richtext`,
+  ],
 };
